491a5b22c3cbf61f3f23e6f4b3419769fda55a4d
407570165 <justinchu0304@gmail.com>
2019-03-18 21:10:20 +0800

    3/18  2100

--- practice/src/tom/com/Tester.java
+++ practice/src/tom/com/Tester.java
@@ -8,11 +8,11 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			List<Integer> rank =new ArrayList<>();
+			/*List<Integer> rank =new ArrayList<>();
 			for(int i =0;i<20;i++) {
 				rank.add(0);
-			}
-			Random random =new Random();
+			}*/
+			/*Random random =new Random();
 			for(int i =0;i<10000;i++) {
 				int r= random.nextInt(20);
 				Integer freq =rank.get(r);
@@ -22,8 +22,19 @@ public class Tester {
 				}else {
 					freq=freq++;
 				}*/
+			//}
+			//System.out.println(rank);
+			List <Integer> list =new ArrayList<>();
+			int[] freqs =new int[20];
+			Random random =new Random();
+			for(int i =0;i<10000;i++) {
+				int r=random.nextInt(20);
+				list.add(r);
+				freqs[r]++;
 			}
-			System.out.println(rank);
+			for(int i=0;i<freqs.length;i++) {
+				System.out.println(list.get(i));
+			}
 	}
 
 }
