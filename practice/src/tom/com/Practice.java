dafd62b64147c17f7c5dc5057f27c05b2137b005
407570165 <justinchu0304@gmail.com>
2019-04-07 15:50:11 +0800

    4/7

---
+++ practice/src/tom/com/Practice.java
@@ -0,0 +1,24 @@
+package tom.com;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList array =new ArrayList<>();
+		ArrayList<Integer> arrays =new ArrayList<Integer>();
+		while(true) {
+		Scanner scanner =new Scanner(System.in);
+		System.out.println("please enter shop name");
+		String name =scanner.nextLine();
+		System.out.println("please enter amount");
+		int am =scanner.nextInt();
+		scanner.nextLine();
+		array.add(name);
+		arrays.add(am);
+		System.out.println(name+" "+am);
+		}
+	}
+}
--- practice/src/tom/com/Tester.java
+++ practice/src/tom/com/Tester.java
@@ -8,27 +8,27 @@ public class Tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			/*List<Integer> rank =new ArrayList<>();
+			List<Integer> rank =new ArrayList<>();
 			for(int i =0;i<20;i++) {
 				rank.add(0);
-			}*/
-			/*Random random =new Random();
+			}
+			Random random =new Random();
 			for(int i =0;i<10000;i++) {
 				int r= random.nextInt(20);
 				Integer freq =rank.get(r);
 				rank.set(r, freq+1);
-				/*if(freq==0) {
+				if(freq==0) {
 					freq=freq+1;
 				}else {
 					freq=freq++;
-				}*/
-			//}
-			//System.out.println(rank);
+				}
+			}
+			System.out.println(rank);
 			List <Integer> list =new ArrayList<>();
 			int[] freqs =new int[20];
-			Random random =new Random();
+			Random randoms =new Random();
 			for(int i =0;i<10000;i++) {
-				int r=random.nextInt(20);
+				int r=randoms.nextInt(20);
 				list.add(r);
 				freqs[r]++;
 			}
