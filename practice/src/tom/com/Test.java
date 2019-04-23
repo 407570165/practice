1450e635e54f2b8f2c0c8eac5322427f599116bf
user <user@es301n04>
2019-04-23 11:58:41 +0800

    2019/4/23

--- practice/src/tom/com/Test.java
+++ practice/src/tom/com/Test.java
@@ -7,11 +7,24 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner scannner =new Scanner(System.in);
-		ArrayList<String> food =new ArrayList<String>();
-		food.add("鮭魚壽司");
-		food.add("鮪魚壽司");
-		System.out.println("your record:"+food);
+		int money=20000;
+		int number =0;
+		Scanner scanner =new Scanner(System.in);
+		System.out.print("how many people:");
+		try{
+		String n =scanner.nextLine();
+		number =Integer.parseInt(n);
+		System.out.println("get: " + (money/number));
+		}
+		catch(Exception e) {
+			System.out.println("錯誤");
+		}
+		/*catch(ArithmeticException E) {
+			System.out.println("數值錯誤");
+		}
+		catch(NumberFormatException e) {
+			System.out.println("格式錯誤");
+		}*/
 	}
 
 }
--- practice/src/tom/com/Test2.java
+++ practice/src/tom/com/Test2.java
@@ -1,16 +1,25 @@
 package tom.com;
 
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Test2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HashSet<String> record =new HashSet<String>();
-		record.add("justin");
-		record.add("hank");
-		record.add("kevin");
-		System.out.println(record);
+	Scanner scanner =new Scanner(System.in);
+	try {
+	System.out.println("輸入數學成績");
+	String m =scanner.nextLine();
+	int math =Integer.parseInt(m);
+	System.out.println("輸入英文成績");
+	int english =Integer.parseInt(scanner.nextLine());
+	int average=(math+english)/2;
+	System.out.println("平均為:" + average);
+	}
+	catch(NumberFormatException e) {
+		System.out.println("格式錯誤");
+	}
+	
 	}
 
 }
