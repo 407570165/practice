f0e8e8da6fd73374e44a9a3748d98b28ac68903e
user <user@es301n05>
2019-05-07 12:04:19 +0800

    5/7

--- practice/src/tom/com/Hello.java
+++ practice/src/tom/com/Hello.java
@@ -40,6 +40,33 @@ public class Hello {
 			}
 		}
 		System.out.println(m);
+		/*List<Integer> rank =new ArrayList<>();
+		for(int i =0;i<20;i++) {
+			rank.add(0);
+		}
+		Random random =new Random();
+		for(int i =0;i<10000;i++) {
+			int r= random.nextInt(20);
+			Integer freq =rank.get(r);
+			rank.set(r, freq+1);
+			if(freq==0) {
+				freq=freq+1;
+			}else {
+				freq=freq++;
+			}
+		}
+		System.out.println(rank);
+		List <Integer> list =new ArrayList<>();
+		int[] freqs =new int[20];
+		Random randoms =new Random();
+		for(int i =0;i<10000;i++) {
+			int r=randoms.nextInt(20);
+			list.add(r);
+			freqs[r]++;
+		}
+		for(int i=0;i<freqs.length;i++) {
+			System.out.println(list.get(i));
+		}*/
 	}
 
 }
--- practice/src/tom/com/Tester.java
+++ practice/src/tom/com/Tester.java
@@ -1,5 +1,8 @@
 package tom.com;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -7,34 +10,24 @@ import java.util.Random;
 public class Tester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			List<Integer> rank =new ArrayList<>();
-			for(int i =0;i<20;i++) {
-				rank.add(0);
-			}
-			Random random =new Random();
-			for(int i =0;i<10000;i++) {
-				int r= random.nextInt(20);
-				Integer freq =rank.get(r);
-				rank.set(r, freq+1);
-				if(freq==0) {
-					freq=freq+1;
-				}else {
-					freq=freq++;
+			File dir =new File("C:\\tmp");
+			if(dir.exists()&& dir.isDirectory()) {
+				File[] files =dir.listFiles();
+				for(File s : files) {
+					if(s.isFile())
+					System.out.println(s.getName());
 				}
 			}
-			System.out.println(rank);
-			List <Integer> list =new ArrayList<>();
-			int[] freqs =new int[20];
-			Random randoms =new Random();
-			for(int i =0;i<10000;i++) {
-				int r=randoms.nextInt(20);
-				list.add(r);
-				freqs[r]++;
-			}
-			for(int i=0;i<freqs.length;i++) {
-				System.out.println(list.get(i));
-			}
+			System.out.println(dir.mkdir());
+			File dd =new File(dir, "aaaaa");
+			System.out.println(dd.mkdirs());
+			
 	}
-
+/*System.out.println(file.length());
+			System.out.println(file.getName());
+			System.out.println(file.getAbsolutePath());
+			System.out.println(file.canExecute());
+			System.out.println(file.exists());
+			System.out.println(file.isFile());*/
+		
 }
